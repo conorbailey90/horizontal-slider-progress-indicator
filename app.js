825394e99c46f264c0db0d48080fb82f71688af5
conorbailey90 <conbailey90@gmail.com>
2021-09-17 21:02:36 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,32 @@
+const slider = document.querySelector('.slider-inner');
+const progressBar = document.querySelector('.prog-bar-inner');
+
+let sliderGrabbed = false;
+
+slider.parentElement.addEventListener('scroll', (e) => {
+    progressBar.style.width  = `${getScrollPercentage()}%`
+})
+
+slider.addEventListener('mousedown', (e) => {
+    sliderGrabbed = true;
+    slider.style.cursor = 'grabbing'
+})
+
+slider.addEventListener('mouseup', (e) => {
+    sliderGrabbed = false;
+    slider.style.cursor = 'grab'
+})
+
+slider.addEventListener('mouseleave', (e) => {
+    sliderGrabbed = false;
+})
+
+slider.addEventListener('mousemove', (e) => {
+    if(sliderGrabbed){
+        slider.parentElement.scrollLeft -= e.movementX
+    }
+})
+
+function getScrollPercentage(){
+   return ((slider.parentElement.scrollLeft / (slider.parentElement.scrollWidth - slider.parentElement.clientWidth) ) * 100).toFixed(0)
+}
